b8a70f78d0743108dd4a82f94d75e9dfe7267370
mackey0209 <seven.km1216@gmail.com>
2020-02-26 09:52:00 +0000

    task4

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get 'home/about'
-  resources :users,only: [:show,:index,:edit,:update,:destroy]
+  resources :users do
+    member do
+     get :following, :followers
+    end
+  end
+
+  resources :relationships,only: [:create, :destroy]
+
   resources :books do
   resource :favorites, only: [:create, :destroy]
-   resources :book_comments, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+
   end
 
 end
